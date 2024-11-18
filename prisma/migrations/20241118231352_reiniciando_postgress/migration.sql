125e539817834fa5f5d000edeb3f33d9951cf877
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-18 20:16:29 -0300

    first commit

---
+++ prisma/migrations/20241118231352_reiniciando_postgress/migration.sql
@@ -0,0 +1,68 @@
+-- CreateEnum
+CREATE TYPE "TipoProduto" AS ENUM ('MONITOR', 'TECLADO', 'MOUSE', 'HEADSET', 'CADEIRA', 'MESA', 'GABINETE', 'PLACA_DE_VIDEO', 'PLACA_MAE', 'PROCESSADOR', 'MEMORIA_RAM', 'HD', 'SSD', 'FONTE', 'COOLER', 'WEBCAM', 'MICROFONE', 'CAIXA_DE_SOM', 'OUTRO');
+
+-- CreateTable
+CREATE TABLE "marcas" (
+    "id" SERIAL NOT NULL,
+    "nome" VARCHAR(30) NOT NULL,
+
+    CONSTRAINT "marcas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "produtos" (
+    "id" SERIAL NOT NULL,
+    "modelo" VARCHAR(60) NOT NULL,
+    "preco" DECIMAL(10,2) NOT NULL,
+    "foto" TEXT NOT NULL,
+    "configuracao" TEXT NOT NULL,
+    "tipo" "TipoProduto" NOT NULL DEFAULT 'OUTRO',
+    "cor" TEXT,
+    "adicional" TEXT,
+    "destacado" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "marcaId" INTEGER NOT NULL,
+
+    CONSTRAINT "produtos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "avaliacoes" (
+    "id" SERIAL NOT NULL,
+    "nota" INTEGER NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "resposta" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "produtoId" INTEGER NOT NULL,
+    "clienteid" TEXT NOT NULL,
+
+    CONSTRAINT "avaliacoes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "clientes" (
+    "id" VARCHAR(36) NOT NULL,
+    "nome" VARCHAR(60) NOT NULL,
+    "email" VARCHAR(60) NOT NULL,
+    "senha" VARCHAR(60) NOT NULL,
+    "foto" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "recuperacao" VARCHAR(6),
+
+    CONSTRAINT "clientes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clientes_email_key" ON "clientes"("email");
+
+-- AddForeignKey
+ALTER TABLE "produtos" ADD CONSTRAINT "produtos_marcaId_fkey" FOREIGN KEY ("marcaId") REFERENCES "marcas"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "avaliacoes" ADD CONSTRAINT "avaliacoes_produtoId_fkey" FOREIGN KEY ("produtoId") REFERENCES "produtos"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "avaliacoes" ADD CONSTRAINT "avaliacoes_clienteid_fkey" FOREIGN KEY ("clienteid") REFERENCES "clientes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
