6368cdea40b306075ec30da9c5926c9643e7f316
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-21 20:12:59 -0300

    Add middleware

--- routes/marcas.ts
+++ routes/marcas.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 import { Router } from "express"
-import { verificaToken } from "../middewares/verificaToken"
+
 
 // const prisma = new PrismaClient()
 const prisma = new PrismaClient({
@@ -41,7 +41,7 @@ router.get("/", async (req, res) => {
   }
 })
 
-router.post("/", verificaToken, async (req, res) => {
+router.post("/", async (req, res) => {
   const { nome } = req.body
 
   if (!nome) {
@@ -59,7 +59,7 @@ router.post("/", verificaToken, async (req, res) => {
   }
 })
 
-router.delete("/:id", verificaToken, async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params
 
   try {
@@ -72,7 +72,7 @@ router.delete("/:id", verificaToken, async (req, res) => {
   }
 })
 
-router.put("/:id", verificaToken, async (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params
   const { nome } = req.body
 
--- routes/produtos.ts
+++ routes/produtos.ts
@@ -1,6 +1,5 @@
 import { PrismaClient, TipoProduto } from "@prisma/client";
 import { Router } from "express";
-import { verificaToken } from "../middewares/verificaToken"
 
 const prisma = new PrismaClient();
 const router = Router();
@@ -30,7 +29,7 @@ router.get("/", async (req, res) => {
 });
 
 
-router.post("/", verificaToken, async (req, res) => {
+router.post("/", async (req, res) => {
   const { modelo, preco, foto, configuracao, tipo, cor, adicional, marcaId } = req.body;
 
   if (!modelo || !preco || !foto || !configuracao || !tipo || !marcaId) {
@@ -59,7 +58,7 @@ router.post("/", verificaToken, async (req, res) => {
   }
 });
 
-router.delete("/:id", verificaToken, async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const produto = await prisma.produto.delete({
@@ -71,7 +70,7 @@ router.delete("/:id", verificaToken, async (req, res) => {
   }
 });
 
-router.put("/:id", verificaToken, async (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { modelo, preco, foto, configuracao, tipo, marcaId } = req.body;
 
